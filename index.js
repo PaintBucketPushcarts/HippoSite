9bfbec63f1f527bb3acb888ede5ccf3ffab8825e
Dbkickva <131828333+Gitkickva@users.noreply.github.com>
2023-12-04 04:07:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function myFunction(){
-let ("dark" == "true");
-let ("light" == "false");
-while ("dark" == "true"){
+let dark = "true";
+let light = "false";
+while dark = "true"{
 document.body.style.backgroundColor = "#000000";
 document.p.style.Color = "#FFFFFF";
 document.h1.style.Color = "#FFFFFF";
@@ -11,9 +11,9 @@ document.a.style.Color = "#FFFFFF";
 }
 }
 function lightMode(){
-let ("light" == "true");
-let ("dark" == "false");
-while ("light" == "true"){
+let light = "true";
+let dark = "false";
+while light = "true"{
 document.body.style.backgroundColor = "#FFFFFF";
 document.p.style.Color = "#000000";
 document.h1.style.Color = "#000000";

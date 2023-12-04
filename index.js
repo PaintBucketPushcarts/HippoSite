4554ac3ef8c1b42314e2820f152f55278a7b3a73
Dbkickva <131828333+Gitkickva@users.noreply.github.com>
2023-12-04 03:22:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function myFunction(){
+let ("dark" == "true");
+document.body.style.backgroundColor = "#000000";
+document.p.style.Color = "#FFFFFF";
+document.h1.style.Color = "#FFFFFF";
+document.h2.style.Color = "#FFFFFF";
+document.a.style.Color = "#FFFFFF";
+}

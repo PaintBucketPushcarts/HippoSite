ccb8dbb8433a71c74d6164ffd800e4c9ab8f06f2
Dbkickva <131828333+Gitkickva@users.noreply.github.com>
2023-12-04 03:26:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
 function myFunction(){
 let ("dark" == "true");
+while ("dark" == "true"){
 document.body.style.backgroundColor = "#000000";
 document.p.style.Color = "#FFFFFF";
 document.h1.style.Color = "#FFFFFF";
 document.h2.style.Color = "#FFFFFF";
 document.a.style.Color = "#FFFFFF";
 }
+}
+function lightMode(){
+document.body.style.backgroundColor = "#FFFFFF";
+document.p.style.Color = "#000000";
+document.h1.style.Color = "#000000";
+document.h2.style.Color = "#000000";
+document.a.style.Color = "#000000";
